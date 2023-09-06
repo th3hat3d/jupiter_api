02770f042d0402b6e5bc851dc0fd841beac42808
th3hat3d <github.rdyhh@simplelogin.com>
2023-09-05 23:27:54 -0400

    added main files

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from flask import Flask,request,Response
+from jupiter_api import *
+app = Flask(__name__)
+@app.route("/api/list/classes",methods=['POST'])
+def list_the_classes():
+ if request.method == 'POST':
+  if request.form.get('school_year'):
+   return list_classes(request.form.get('user'),request.form.get('pass'), request.form.get('school_year'))
+  else:
+   return list_classes(request.form.get('user'),request.form.get('pass'))
+
+@app.route("/api/profile_picture",methods=['POST'])
+def get_the_image():
+ if request.method == 'POST':
+  return Response(get_picture(request.form.get('user'),request.form.get('pass')), mimetype="image/jpg")
+
+app.run()
---
+++ jupiter_api.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import requests
+
+def list_classes(user,passw,school_year="current"):
+    driver = webdriver.Chrome()
+
+    driver.get("https://login.jupitered.com/login/index.php?89967")
+    driver.implicitly_wait(0.5)
+    student_name = driver.find_element(value="text_studid1")
+    student_name.send_keys(user)
+    password = driver.find_element(value="text_password1")
+    password.send_keys(passw)
+    login_button = driver.find_element(value="loginbtn")
+    login_button.click()
+    time.sleep(2)
+    if school_year != "current":
+        driver.execute_script(f'changeschoolyear("89967,{school_year}")')
+    html_doc = driver.page_source
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    classes = soup.find_all(class_="classnav")
+    class_list = ""
+    for i in classes:
+        class_list += str(BeautifulSoup(str(i), 'html.parser').get_text() + "\n")
+    driver.quit()
+    return class_list
+
+def get_picture(user,passw):
+    driver = webdriver.Chrome()
+
+    driver.get("https://login.jupitered.com/login/index.php?89967")
+    driver.implicitly_wait(0.5)
+    student_name = driver.find_element(value="text_studid1")
+    student_name.send_keys(user)
+    password = driver.find_element(value="text_password1")
+    password.send_keys(passw)
+    login_button = driver.find_element(value="loginbtn")
+    login_button.click()
+    time.sleep(2)
+    driver.execute_script('go("settings")')
+    time.sleep(1)
+    pic_link = driver.find_element(By.CLASS_NAME, "rad12").get_attribute("src")
+    driver.quit()
+    return requests.get(pic_link).content
