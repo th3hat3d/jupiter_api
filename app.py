35a8db5dc1e903f2fc96673a041e5fefad75338b
th3hat3d <github.rdyhh@simplelogin.com>
2023-09-26 23:09:07 -0400

    added ability to list grades

--- app.py
+++ app.py
@@ -15,4 +15,11 @@ def get_the_image():
  if request.method == 'POST':
   return Response(get_picture(request.form.get('user'),request.form.get('pass')), mimetype="image/jpg")
 
+@app.route("/api/list/grades",methods=['POST'])
+def list_the_grades():
+ if request.method == 'POST':
+  if request.form.get('school_year'):
+   return list_grades(request.form.get('user'),request.form.get('pass'), request.form.get('school_year'))
+  else:
+   return list_grades(request.form.get('user'),request.form.get('pass'))
 app.run()
--- jupiter_api.py
+++ jupiter_api.py
@@ -44,3 +44,28 @@ def get_picture(user,passw):
     pic_link = driver.find_element(By.CLASS_NAME, "rad12").get_attribute("src")
     driver.quit()
     return requests.get(pic_link).content
+
+def list_grades(user,passw,school_year="current"):
+    driver = webdriver.Chrome()
+
+    driver.get("https://login.jupitered.com/login/index.php?89967")
+    driver.implicitly_wait(0.5)
+    student_name = driver.find_element(value="text_studid1")
+    student_name.send_keys(user)
+    password = driver.find_element(value="text_password1")
+    password.send_keys(passw)
+    login_button = driver.find_element(value="loginbtn")
+    login_button.click()
+    time.sleep(2)
+    if school_year != "current":
+        driver.execute_script(f'changeschoolyear("89967,{school_year}")')
+    driver.execute_script('go("todo")')
+    html_doc = driver.page_source
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    l = ""
+    for i in soup.find_all(style="padding:8px 20px 9px 20px"):
+        if i.get_text().strip().partition(" \xa0")[0] == "":
+            l += "N/A\n"
+        else:
+            l += (i.get_text().strip().partition(" \xa0")[0] + "\n")
+    return l
